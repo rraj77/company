bc4cf6b1311a5f2b7df770650109b1c994b374bd
simranmalviya123 <simran.malviya@backbenchertechnologies.com>
2023-03-01 18:35:34 +0530

    change in drive features

--- src/drive/Drive.tsx
+++ src/drive/Drive.tsx
@@ -1,19 +1,104 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
+import { Modal, Typography, TextField } from '@mui/material';
+import style from '../styles/drive.module.scss';
 import DisplayCard from './DisplayCard';
+import audio from '../image/audio.png';
+import video from '../image/video.png';
+import image from '../image/image.png';
+import natureimage from '../image/nature.webp';
+import zipimage from '../image/zip.png';
+import wallpaper from '../image/wallpaper.jpg';
+import filefolder from '../image/filefolder.png';
+
+const driveCardDetails = [
+  { title: 'audio.mp3', image: audio },
+  { title: 'video.mp4', image: video },
+  { title: 'image.png', image: image },
+  { title: 'nature.webp', image: natureimage },
+  { title: 'video.mp4', image: video },
+  { title: 'zip.png', image: zipimage },
+  { title: 'wallpaper', image: wallpaper },
+  { title: 'audio.mp3', image: audio },
+  { title: 'image.png', image: image },
+  { title: 'audio.mp3', image: audio },
+  { title: 'video.mp4', image: video },
+  { title: 'image.png', image: image },
+  { title: 'nature.webp', image: natureimage },
+  { title: 'video.mp4', image: video },
+  { title: 'zip.png', image: zipimage },
+  { title: 'audio.mp3', image: audio }
+];
 
 function Drive() {
-  return (
-    <Box textAlign={'end'}>
-      <Button variant="contained" size="small">
-        Upload
-      </Button>
+  const [open, setOpen] = useState(false);
+  const [name, setName] = useState<string>('');
 
-      <Box>
-        <DisplayCard />
+  const ModelClose = () => {
+    setOpen(false);
+    setName('')
+  };
+
+  const getName = (name: string) => {
+    const folder: { title: string; image: string } = {
+      title: name,
+      image: filefolder
+    };
+    driveCardDetails.unshift(folder);
+    ModelClose();
+    setName('');
+  };
+
+  return (
+    <>
+      <Modal
+        open={open}
+        onClose={ModelClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+        className={style.modal}>
+        <Box className={style.popup_style}>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Folder Name
+          </Typography>
+          <TextField
+            id="name-input"
+            name="name"
+            type="text"
+            value={name}
+            size="small"
+            onChange={(e) => setName(e.target.value)}
+          />
+          <br />
+          <Box sx={{ paddingTop: '7px' }}>
+            <Button
+              variant="contained"
+              onClick={ModelClose}
+              color="error"
+              sx={{ marginRight: '7px' }}>
+              Cancel
+            </Button>
+            <Button variant="contained" onClick={() => getName(name)} color="success">
+              Success
+            </Button>
+          </Box>
+        </Box>
+      </Modal>
+      <Box sx={{ textAlign: 'end', display: 'flex', justifyContent: 'space-between' }}>
+        <Button
+          variant="contained"
+          onClick={() => setOpen(true)}
+          size="small"
+          className={style.create_button}>
+          Create Folder
+        </Button>
+        <Button variant="contained" size="small" className={style.upload_button}>
+          Upload
+        </Button>
       </Box>
-    </Box>
+      <DisplayCard driveCardDetails={driveCardDetails} />
+    </>
   );
 }
 export default Drive;
--- src/drive/DriveCard.tsx
+++ src/drive/DriveCard.tsx
@@ -1,5 +1,16 @@
 import React from 'react';
-import { Card, CardMedia, CardContent, Typography } from '@mui/material';
+import {
+  Card,
+  CardMedia,
+  CardContent,
+  Typography,
+  Menu,
+  MenuItem,
+  Button,
+  IconButton
+} from '@mui/material';
+import style from '../styles/drive.module.scss';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 interface DriveCardProps {
   cardDetails: {
@@ -9,14 +20,76 @@ interface DriveCardProps {
 }
 
 const DriveCard = ({ cardDetails }: DriveCardProps) => {
+  const [toggleButton, setToggleButton] = React.useState<boolean>(false);
+
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  function mouseInHandler() {
+    setToggleButton(true);
+  }
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const mouseOutHandler = () => {
+    setToggleButton(false);
+  };
+
   return (
     <>
-      <Card sx={{ maxWidth: 200, maxHeight: 160, fontSize: 2 }}>
+      <Card
+        className={style.card}
+        sx={{ position: 'relative' }}
+        aria-owns={anchorEl ? 'simple-menu' : undefined}
+        aria-haspopup="true"
+        onMouseEnter={mouseInHandler}
+        onMouseLeave={mouseOutHandler}>
         <CardMedia sx={{ height: 110 }} image={cardDetails.image} title={cardDetails.title} />
-        <CardContent>
+
+        <CardContent
+          sx={{
+            height: '10px',
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center'
+          }}>
           <Typography gutterBottom variant="body2" component="div">
             {cardDetails.title}
           </Typography>
+          {toggleButton ? (
+            <>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="inherit">
+                <MoreVertIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right'
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right'
+                }}
+                open={Boolean(anchorEl)}
+                onClose={handleClose}>
+                <MenuItem onClick={handleClose}>Download</MenuItem>
+                <MenuItem onClick={handleClose}>Delete</MenuItem>
+              </Menu>
+            </>
+          ) : null}
         </CardContent>
       </Card>
     </>
